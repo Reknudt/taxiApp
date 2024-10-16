53977bf300d23eea283a177eb2c51bdc6c425991
Reknudt <pochta.navsegda@mail.ru>
2024-10-17 00:27:31 +0300

    Fix spaces

--- driver-service/src/main/java/com/kpavlov/moduledrivercarservice/dto/request/update/DriverUpdateRequest.java
+++ driver-service/src/main/java/com/kpavlov/moduledrivercarservice/dto/request/update/DriverUpdateRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record DriverUpdateRequest(
+public record DriverUpdateRequest (
 
         @NotBlank(message = "{field.required}")
         String name,
--- driver-service/src/main/java/com/kpavlov/moduledrivercarservice/exception/DuplicateFoundException.java
+++ driver-service/src/main/java/com/kpavlov/moduledrivercarservice/exception/DuplicateFoundException.java
@@ -1,6 +1,6 @@
 package com.kpavlov.moduledrivercarservice.exception;
 
-public class DuplicateFoundException extends RuntimeException{
+public class DuplicateFoundException extends RuntimeException {
 
     public DuplicateFoundException(String message) {
         super(message);
